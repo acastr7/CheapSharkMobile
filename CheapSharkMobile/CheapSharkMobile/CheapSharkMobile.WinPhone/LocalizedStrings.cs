239151cb8ab652f1d05961fa1289197e7532e3d1
Andres <acastro@bluetubeinc.com>
2015-04-04 18:35:25 -0400

    Initial Solution Commit.

---
+++ CheapSharkMobile/CheapSharkMobile/CheapSharkMobile.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using CheapSharkMobile.WinPhone.Resources;
+
+namespace CheapSharkMobile.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
