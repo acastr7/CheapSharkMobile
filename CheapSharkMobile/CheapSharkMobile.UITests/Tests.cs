c874569bee778a5892c8e2431b183a3fb4db282a
Andres Castro <acastr7@gmail.com>
2015-12-01 20:42:54 -0500

    Update to Xamarin Forms 2.0

--- CheapSharkMobile/CheapSharkMobile.UITests/Tests.cs
+++ CheapSharkMobile/CheapSharkMobile.UITests/Tests.cs
@@ -3,28 +3,14 @@ using System.IO;
 using System.Linq;
 using NUnit.Framework;
 using Xamarin.UITest;
-using Xamarin.UITest.Android;
 using Xamarin.UITest.Queries;
 
-namespace CheapSharkMobile.UITest
+namespace CheapSharkMobile.UITests
 {
-	public class AppInitializer
-	{
-		public static IApp StartApp (Platform platform)
-		{
-			if (platform == Platform.Android) {
-				return ConfigureApp.Android.StartApp ();
-			}
-
-			return ConfigureApp.iOS.StartApp ();
-		}
-	}
-
 	[TestFixture (Platform.Android)]
 	[TestFixture (Platform.iOS)]
 	public class Tests
 	{
-
 		IApp app;
 		Platform platform;
 
